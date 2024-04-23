573b6481f70c27cbb4e81da76e0940a48bf0206b
hari11918 <49097224+hari11918@users.noreply.github.com>
2024-04-23 01:02:20 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css'; 
+import EmployeeManager from './EmployeeManager'; 
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeManager />
+    </div>
+  );
+}
+
+export default App;
